32ec1fb2e6a21fa58d925a7bf4cfb4e8cbc27267
Aksel Arzuman <akselarzuman@gmail.com>
2018-05-20 20:11:43 +0300

    LukeBuilder added

---
+++ src/Luke.Core/Contracts/ILukeBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Luke.Core.Base;
+
+namespace Luke.Core.Contracts
+{
+    public interface ILukeBuilder
+    {
+        Task BuildAsync(string path);
+    }
+}
---
+++ src/Luke.Core/LukeBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Luke.Core.Base;
+using Luke.Core.Contracts;
+using Luke.Exceptions;
+using Luke.Models;
+using Quartz;
+
+namespace Luke.Core
+{
+    public class LukeBuilder : ILukeBuilder
+    {
+        private readonly ILuker _luker;
+        private readonly ISchedulerFactory _schedulerFactory;
+
+        public LukeBuilder(ILuker luker, ISchedulerFactory schedulerFactory)
+        {
+            _luker = luker;
+            _schedulerFactory = schedulerFactory;
+        }
+        
+        public async Task BuildAsync(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ParameterRequiredException(nameof(path));
+            }
+            
+            IEnumerable<LukePkgModel> lukePkgModels = await _luker.ReadPkgAsync(path);
+            IEnumerable<LukeLocationModel> lukeLocationModels = await _luker.LocateAsync(lukePkgModels);
+
+            foreach (LukeLocationModel lukeLocationModel in lukeLocationModels)
+            {
+                bool isValidAssembly = await _luker.IsValidAssembly(lukeLocationModel);
+                if (isValidAssembly)
+                {
+                    await _luker.Load(lukeLocationModel);
+                }
+            }
+        }
+    }
+}
