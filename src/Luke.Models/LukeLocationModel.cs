146f1d7bcc28ac3a4a7ca083ae32c365b66a2d5f
Aksel Arzuman <akselarzuman@gmail.com>
2018-05-20 19:41:25 +0300

    LukeLocation and LukePkg models added

---
+++ src/Luke.Models/LukeLocationModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Luke.Models
+{
+    public class LukeLocationModel
+    {
+        public string AssemblyName { get; set; }
+        public string AssemblyLocation { get; set; }
+    }
+}
